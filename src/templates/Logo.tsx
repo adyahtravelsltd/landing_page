d06e1bf4e4c512acd78788a88d4b1054efdcf8a8
Deepak Chauhan <127858338+deechauhan@users.noreply.github.com>
2024-03-17 19:05:08 +0000

    change logo path

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -12,7 +12,7 @@ const Logo = (props: ILogoProps) => {
   return (
     <span className={`inline-flex items-center text-gray-900 ${fontStyle}`}>
       <Image
-        src="/assets/images/logo.png"
+        src="/landing_page/assets/images/logo.png"
         alt="Adyah Travels"
         width={200}
         height={100}
